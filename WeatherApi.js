c83ea3117f2ed6c44ca438d39d8ba0c9ebc12908
Harsh050804 <110337377+Harsh050804@users.noreply.github.com>
2024-07-25 08:48:34 +0530

    Create WeatherApi.js

---
+++ WeatherApi.js
@@ -0,0 +1,23 @@
+import Axios from "axios";
+
+const weather_api_url = "https://api.open-meteo.com/v1/forecast";
+const convertDate = (date) =>{
+const year = date.getFullYear();
+const month = date.getMonth()+1 <= 9 ? `0${date.getMonth()+1}` : date.getmonth() + 1 ;
+const day = date.getDate() <= 9 ? `0${date.getDate()}` : date.getDate() ;
+return `${year}-${month}-${day}`;
+}
+const getWeather = async (latitude, longitude) => {
+const currentDate = new Date();
+const startDate = new Date(currentDate.setDate(currentDate.getDate()+1));
+const {data: WeatherInfo} = await Axios.get(weather_api_url, {
+  params: {
+    latitude, longitude, current_weather: true, timezone: "IST", daily: ["temperature_2m_max","temperature_2m_min","weathercode"],
+    start_Date: convertDate(startDate),
+    end_date: convertDate(new Date(startDate.setDate(startDate.getDate() + 7))),
+  },
+});
+return WeatherInfo;
+}
+
+export default getWeather;
